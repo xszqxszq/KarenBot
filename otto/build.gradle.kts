b010f3c41de534dd6b5e2dc8bcf3183acaec4d52
xszqxszq <943551369@qq.com>
2026-04-01 17:16:52 +0800

    Fix dependency cannot be downloaded

--- otto/build.gradle.kts
+++ otto/build.gradle.kts
@@ -11,11 +11,13 @@ version = "1.0"
 repositories {
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
-//    maven { url = uri("https://raw.githubusercontent.com/DFKI-MLT/Maven-Repository/main") }
+    maven { url = uri("https://raw.githubusercontent.com/DFKI-MLT/Maven-Repository/main") }
     maven { url = uri("https://nrgxnat.jfrog.io/artifactory/libs-release/") }
     maven { url = uri("https://nexus.terrestris.de/repository/public/") }
 }
-
+configurations.all {
+    exclude(group = "gov.nist.math", module = "Jampack")
+}
 dependencies {
     compileOnly(project(":"))
     implementation("com.hankcs:hanlp:portable-1.8.6")
