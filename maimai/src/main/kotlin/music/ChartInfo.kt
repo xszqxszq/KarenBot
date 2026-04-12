232ca449dc5c8191aa96acf73e97b0bc38118cf4
xszqxszq <943551369@qq.com>
2026-04-12 14:42:26 +0800

    Change chart info style; Add chart preview link

--- maimai/src/main/kotlin/music/ChartInfo.kt
+++ maimai/src/main/kotlin/music/ChartInfo.kt
@@ -1,11 +1,11 @@
 package xyz.xszq.bot.music
 
-import io.ktor.http.*
 import korlibs.io.util.toStringDecimal
 import xyz.xszq.bot.component.MarkdownTemplates.href
 import xyz.xszq.bot.message.Image
 import xyz.xszq.bot.message.Markdown
 import xyz.xszq.bot.newLine
+import xyz.xszq.bot.payload.markdown.Keyboard
 import xyz.xszq.bot.payload.markdown.MarkdownData
 import xyz.xszq.bot.plus
 
@@ -43,12 +43,18 @@ class ChartInfo(
     ) = Markdown(MarkdownData(buildString {
         appendLine("![img#190px #190px]($jacketUrl/${music.resourceId}.jpg)")
         appendLine("**${difficulty.names.last()}${music.id}. ${music.name}**")
+        appendLine("**曲师:** ${href("曲师查歌 ${music.artist}", music.artist)}")
+        appendLine("**分类:** ${href("${music.genre.genreName}有什么歌", music.genre.genreName)}")
+        appendLine("**版本:** ${href("版本查歌 ${music.version.name}", music.version.name)}")
+        appendLine("**BPM:** ${href("BPM查歌 ${music.bpm}", music.bpm.toString())}")
+        appendLine("**等级:** $level (${href("定数查歌 $levelValue", levelValue.toString())})")
+        val designer = notesDesigner.ifBlank { "-" }
+        appendLine("**谱师:** ${href("谱师查歌 $designer", designer)}")
+        if (fitLevelValue != 0.0) {
+            val value = fitLevelValue.toStringDecimal(1)
+            appendLine("**拟合定数:** ${href("拟合定数查歌 $value", value)}")
+        }
         appendLine()
-        appendLine("> **曲师:** ${href("曲师查歌 ${music.artist}", music.artist)}")
-        appendLine("> **分类:** ${href("${music.genre.genreName}有什么歌", music.genre.genreName)}")
-        appendLine("> **版本:** ${href("版本查歌 ${music.version.name}", music.version.name)}")
-        appendLine("> **BPM:** ${href("BPM查歌 ${music.bpm}", music.bpm.toString())}")
-        appendLine("> **等级:** $level (${href("定数查歌 $levelValue", levelValue.toString())})")
         appendLine("> **TAP:** ${notes.tap}")
         appendLine("> **HOLD:** ${notes.hold}")
         appendLine("> **SLIDE:** ${notes.slide}")
@@ -57,11 +63,9 @@ class ChartInfo(
             appendLine("> **TOUCH:** ${notes.touch}")
         appendLine("> **总物量:** ${notes.total}")
         appendLine("> **总DX分:** $maxDeluxeScore")
-        val designer = notesDesigner.ifBlank { "-" }
-        appendLine("> **谱师:** ${href("谱师查歌 $designer", designer)}")
-        if (fitLevelValue != 0.0) {
-            val value = fitLevelValue.toStringDecimal(1)
-            appendLine("> **拟合定数:** ${href("拟合定数查歌 $value", value)}")
+    }), Keyboard.create {
+        row {
+            link("谱面确认", "https://otmdb.cn/jump/maimai_chart?chart_id=${music.resourceId}&difficulty=${difficulty.value}")
         }
-    }))
+    })
 }
