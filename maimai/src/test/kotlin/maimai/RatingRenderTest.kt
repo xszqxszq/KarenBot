868cb754d89495f02c0973f5f8994fa64ac4f48f
xszqxszq <943551369@qq.com>
2026-07-30 15:56:24 +0800

    Add test for best 50 in maimai

---
+++ maimai/src/test/kotlin/maimai/RatingRenderTest.kt
@@ -0,0 +1,37 @@
+package xyz.xszq.bot.maimai
+
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import xyz.xszq.bot.*
+import xyz.xszq.bot.maimai.database.MaimaiDatabaseTest
+import xyz.xszq.bot.payload.UploadResult
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class RatingRenderTest : MaimaiDatabaseTest() {
+
+    @Test
+    fun testB50MaxScore() = runTest {
+        val cos = mockk<TencentCos>(relaxed = true)
+        coEvery { cos.uploadBinary(any(), any()) } returns UploadResult("https://example.com/b50.jpg", "b50.jpg")
+
+        val sandbox = BotSandbox(this, cos, database)
+
+        val maimai = Maimai().apply {
+            plugin = "maimai"
+            pluginLoader = sandbox.pluginLoader
+            configPath = "../config/maimai.yml"
+            dataPath = "../data/maimai"
+        }
+        maimai.load()
+
+        val msg = sandbox.user() says "/b50 maxscore"
+
+        assertNotNull(sandbox.replyFor(msg)) {
+            "Reply is null, replies.size=${sandbox.replies.size}"
+        }
+    }
+}
