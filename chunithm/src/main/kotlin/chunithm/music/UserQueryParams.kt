d22bbca9efef7c8dafa8ebbc1d5fd0d22770bb95
xszqxszq <943551369@qq.com>
2026-09-01 12:07:23 +0800

    Refactor UserQueryParams to openid identity

--- chunithm/src/main/kotlin/chunithm/music/UserQueryParams.kt
+++ chunithm/src/main/kotlin/chunithm/music/UserQueryParams.kt
@@ -7,16 +7,20 @@ sealed class UserQueryParams(
     open val isSelf: Boolean,
     open val settings: PlayerSettings?
 ) {
-    data class QQ(
-        val qq: Long,
+    data class Self(
         override val event: MessageEvent,
-        override val isSelf: Boolean,
         override val settings: PlayerSettings? = null
-    ) : UserQueryParams(event, isSelf, settings)
+    ) : UserQueryParams(event, true, settings)
     data class Username(
         val username: String,
         override val event: MessageEvent,
-        override val isSelf: Boolean,
+        override val isSelf: Boolean = false,
+        override val settings: PlayerSettings? = null
+    ) : UserQueryParams(event, isSelf, settings)
+    data class FriendCode(
+        val friendCode: String,
+        override val event: MessageEvent,
+        override val isSelf: Boolean = false,
         override val settings: PlayerSettings? = null
     ) : UserQueryParams(event, isSelf, settings)
 }
